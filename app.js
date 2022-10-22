1cc1bd8e17dcedf0555ec4d05273df537ba1622c
karthikeyan.p <96372362+KarthiknPK@users.noreply.github.com>
2022-10-22 22:13:19 +0530

    lim

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const DBSOURCE = "usersdb.sqlite";
 const auth = require("./middleware");
 var bodyParser = require('body-parser');
 
-app.use(express.json());
+app.use(express.json({limit:'50mb'}));
 app.use(express.urlencoded({extended: true}));
 
 
